014c021960907f91de0ed5f642f781f485179737
Soslan <sparksoslan@gmail.com>
2020-05-15 14:58:22 +0100

    Directory sorted and provided assets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import JobCard from './components/JobCard/JobCard.js';
+import jobData from './assets/data';
 
 function App() {
+  const [data, setData] = useState(jobData);
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>APP</h1>
+      <JobCard />
     </div>
   );
 }
---
+++ src/components/JobCard/JobCard.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const JobCard = () => {
+  return (
+    <div>
+      <h2>Job Card</h2>
+    </div>
+  );
+};
+
+export default JobCard;
