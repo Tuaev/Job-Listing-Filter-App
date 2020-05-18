16c04c600ffae874c3fad39010e0582599284d18
Soslan <sparksoslan@gmail.com>
2020-05-18 20:10:53 +0100

    Add mobile reponsiveness and filter by tag functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
 import React, { useState, useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
 
 import JobCard from './components/JobCard/JobCard.js';
 import data from './assets/data';
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     setJobs(data);
   }, []);
 
+  const filteredJobs = jobs.filter((job) => {
+    if (filters.length === 0) {
+      return true;
+    }
+    const jobTags = [job.role, job.level, ...job.languages, ...job.tools];
+    return jobTags.some((tag) => filters.includes(tag));
+  });
+
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) {
+      return;
+    }
+    return setFilters([...filters, tag]);
+  };
+
+  const removeFilterTag = (tag) => {
+    setFilters(filters.filter((f) => f !== tag));
+  };
+  const clearFilters = () => {
+    setFilters([]);
+  };
+
   return (
-    <div className="bg-blue-100">
-      <h1 className="text-4xl">APP</h1>
-      {jobs.length === 0 ? (
-        <p>Loading...</p>
-      ) : (
-        jobs.map((job) => <JobCard key={job.id} job={job} />)
-      )}
-    </div>
+    <React.Fragment>
+      <header className="bg-teal-500 mb-24">
+        <img className="object-cover h-48 w-full " src="/images/bg-header-desktop.svg" alt="" />
+      </header>
+      <div className="container m-auto">
+        {filters.length > 0 && (
+          <div
+            className={
+              'relative flex flex-wrap lg:flex-row bg-white shadow-lg -mt-32 mx-8 p-6 rounded z-5'
+            }
+          >
+            {filters.map((filterTag) => (
+              <React.Fragment key={uuid()}>
+                <span
+                  onClick={() => removeFilterTag(filterTag)}
+                  className="cursor-pointer text-teal-500 bg-blue-100 p-2 rounded-l text-sm font-bold"
+                >
+                  {filterTag}
+                </span>
+                <span
+                  onClick={() => removeFilterTag(filterTag)}
+                  className="cursor-pointer hover:bg-gray-900  bg-teal-500 p-2 rounded-r text-sm font-bold mr-3"
+                >
+                  <img className="mt-1" src="/images/icon-remove.svg" alt="Remove" />
+                </span>
+              </React.Fragment>
+            ))}
+            <button
+              onClick={() => clearFilters()}
+              className="ml-auto font-bold text-gray-600 cursor-pointer hover:underline hover:text-teal-500"
+            >
+              Clear
+            </button>
+          </div>
+        )}
+        {jobs.length === 0 ? (
+          <p>Loading...</p>
+        ) : (
+          filteredJobs.map((job) => (
+            <JobCard key={job.id} job={job} handleTagClick={handleTagClick} />
+          ))
+        )}
+      </div>
+    </React.Fragment>
   );
 }
 
--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -1,27 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import styles from './JobCardStyles.js';
 
-const JobCard = ({ job }) => {
-  // const { classes, jobs } = props;
-  const langAndTools = [...job.languages, ...job.tools];
-  // console.log(langAndTools);
+const JobCard = ({ job, handleTagClick }) => {
+  const jobTags = [job.role, job.level, ...job.languages, ...job.tools];
+
   return (
-    <div className="flex bg-white shadow-md m-4 p-6">
+    <div
+      className={`flex flex-col lg:flex-row bg-white shadow-lg my-16 lg:my-8 mx-8 p-6 rounded ${
+        job.featured && 'border-l-4 border-teal-500 border-solid'
+      }`}
+    >
       <div>
-        <img src={job.logo} alt={job.company}></img>
+        <img
+          className="w-20 h-20 lg:w-24 lg:h-24 -mt-16 mb-4 lg:mt-0 lg:mb-0"
+          src={job.logo}
+          alt={job.company}
+        ></img>
       </div>
-      <div className="flex flex-col justify-between ml-5">
-        <h3 className="font-bold text-teal-500">{job.company}</h3>
-        <h2 className="font-bold text-xl">{job.position}</h2>
-        <p className="text-gray-500">
-          {job.postedAt} &#183; {job.contract} &#183; {job.location}
-        </p>
+      <div className="flex flex-col justify-between lg:ml-4">
+        <h3 className="text-teal-500">
+          <span className="mr-3 font-semibold">{job.company}</span>
+          {job.new && (
+            <span className="font-medium text-sm rounded-full py-1 px-2 m-1 bg-teal-600 text-blue-100">
+              NEW!
+            </span>
+          )}
+          {job.featured && (
+            <span className="font-medium text-sm rounded-full py-1 px-2 m-1 bg-gray-900 text-blue-100">
+              FEATURED
+            </span>
+          )}
+        </h3>
+        <h2 className="font-bold text-xl my-2 lg:my-0 hover:text-teal-500 cursor-pointer">
+          {job.position}
+        </h2>
+        <div className="text-gray-500 font-light">
+          <span className="lg:pr-3 pr-1">{job.postedAt}</span>
+          <span className="lg:pr-3 pr-1 font-black">&#183; </span>
+          <span className="lg:pr-3 pr-1">{job.contract}</span>
+          <span className="lg:pr-3 pr-1 font-black">&#183; </span>
+          <span>{job.location}</span>
+        </div>
       </div>
-      <div className="flex items-center ml-auto">
-        {langAndTools &&
-          langAndTools.map((langAndTool, i) => (
-            <span className="text-teal-500 bg-blue-100 m-1 p-2 rounded text-sm font-bold" key={i}>
-              {langAndTool}
+      <div className="flex flex-wrap items-center mr-4 mt-6 pt-4 border-t border-gray-500 border-solid lg:ml-auto lg:border-0 lg:pt-0 lg:mt-0">
+        {jobTags &&
+          jobTags.map((jobTag, i) => (
+            <span
+              className="cursor-pointer text-teal-500 bg-blue-100 mr-4 my-2 p-2 rounded text-sm font-bold"
+              key={i}
+              onClick={() => handleTagClick(jobTag)}
+            >
+              {jobTag}
             </span>
           ))}
       </div>
